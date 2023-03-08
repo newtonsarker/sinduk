c0b1d4c50deea0a2999a79f1fc6dd04c60951ea6
Newton  Sarker <newtonsarker@gmail.com>
2023-03-08 20:05:55 +0100

    Added documentation to the class FileUtil

--- app/src/main/java/io/ns/sinduk/utils/FileUtil.java
+++ app/src/main/java/io/ns/sinduk/utils/FileUtil.java
@@ -7,12 +7,27 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
+/**
+ * A utility to perform file based operations
+ */
 public class FileUtil {
 
+    /**
+     * Checks whether the file exists or not
+     *
+     * @param fqfn The fully qualified file name to check in the system
+     * @return True if the file exists, false otherwise
+     */
     public static boolean fileExists(String fqfn) {
         return new File(fqfn).exists();
     }
 
+    /**
+     * Removes the file from the sytem if exists
+     *
+     * @param fqfn The fully qualified file name to be deleted
+     * @return True if the delete operation succeeds, false otherwise
+     */
     public static boolean deleteFile(String fqfn) {
         if (fileExists(fqfn)) {
             return new File(fqfn).delete();
@@ -20,6 +35,13 @@ public class FileUtil {
         return false;
     }
 
+    /**
+     * Write the given content to a file
+     *
+     * @param fqfn The fully qualified file name to write the content
+     * @param content The content to write in the file
+     * @throws IOException If any error occurs during file writing operation
+     */
     public static void writeToFile(String fqfn, String content) throws IOException {
         File file = new File(fqfn);
         file.getParentFile().mkdirs();
@@ -29,6 +51,13 @@ public class FileUtil {
         }
     }
 
+    /**
+     * Read the content from a given file name
+     *
+     * @param fqfn The fully qualified file name to read the content from
+     * @return The content of the given file name
+     * @throws IOException If any error occurs during file read operation
+     */
     public static String readFromFile(String fqfn) throws IOException {
         File file = new File(fqfn);
         try (FileInputStream fis = new FileInputStream(file)) {

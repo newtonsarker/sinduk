f3a5ea88a5a352a30cb563e2795a2c7152c01dc9
Newton  Sarker <newtonsarker@gmail.com>
2023-03-08 19:06:33 +0100

    Added documentation to the class PublicKeyUtil

--- app/src/main/java/io/ns/sinduk/utils/PublicKeyUtil.java
+++ app/src/main/java/io/ns/sinduk/utils/PublicKeyUtil.java
@@ -14,19 +14,33 @@ import java.security.spec.X509EncodedKeySpec;
 
 /**
  * This class provides an example of how to encrypt and decrypt a message
- * using public key encryption.
+ * using public key encryption
  */
 public class PublicKeyUtil {
 
     private static final String ALGORITHM = "RSA";
     private static final int KEY_SIZE = 2048;
 
+    /**
+     * Generate a new public/private key pair
+     *
+     * @return The public/private key pair
+     * @throws GeneralSecurityException If there is an error during key pair generation
+     */
     public static KeyPair generateKeyPair() throws GeneralSecurityException {
         KeyPairGenerator keyGen = KeyPairGenerator.getInstance(ALGORITHM);
         keyGen.initialize(KEY_SIZE, SecureRandom.getInstanceStrong());
         return keyGen.generateKeyPair();
     }
 
+    /**
+     * Encrypts the given message using the provided public key
+     *
+     * @param encodedPublicKey The private key to use for encryption
+     * @param message The message to encrypt.
+     * @return The encrypted message
+     * @throws GeneralSecurityException If there is an error during encryption
+     */
     public static String encrypt(String encodedPublicKey, String message) throws GeneralSecurityException {
         byte[] publicKeyBytes = Base64Util.decodeToBytes(encodedPublicKey);
         PublicKey publicKey = KeyFactory.getInstance(ALGORITHM).generatePublic(new X509EncodedKeySpec(publicKeyBytes));
@@ -34,6 +48,14 @@ public class PublicKeyUtil {
         return Base64Util.encodeToString(encryptedMessage);
     }
 
+    /**
+     * Decrypts the given encrypted message using the provided private key
+     *
+     * @param encodedPrivateKey The private key to use for decryption
+     * @param encodedEncryptedMessage The encrypted message to decrypt
+     * @return The decrypted message
+     * @throws GeneralSecurityException If there is an error during decryption
+     */
     public static String decrypt(String encodedPrivateKey, String encodedEncryptedMessage) throws GeneralSecurityException {
         byte[] privateKeyBytes = Base64Util.decodeToBytes(encodedPrivateKey);
         PrivateKey privateKey = KeyFactory.getInstance(ALGORITHM).generatePrivate(new PKCS8EncodedKeySpec(privateKeyBytes));
@@ -42,6 +64,15 @@ public class PublicKeyUtil {
         return new String(decryptedMessage);
     }
 
+    /**
+     * Create key cipher to encrypt or decrypt plain text
+     *
+     * @param key The PublicKey or PrivateKey
+     * @param cipherMode The mode for generating the cipher, whether to be used for encryption or decryption
+     *                   Values could be {@code ENCRYPT_MODE} or {@code DECRYPT_MODE}
+     * @return The cipher
+     * @throws GeneralSecurityException If there is an error during cipher generation
+     */
     private static Cipher createCipher(Key key, int cipherMode) throws GeneralSecurityException {
         Cipher cipher = Cipher.getInstance(ALGORITHM);
         cipher.init(cipherMode, key);

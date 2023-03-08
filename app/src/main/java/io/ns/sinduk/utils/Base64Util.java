812a099cfe52c372cdb678332a25c81b15bb8e5d
Newton  Sarker <newtonsarker@gmail.com>
2023-03-08 19:54:03 +0100

    Added documentation to the class Base64Util

--- app/src/main/java/io/ns/sinduk/utils/Base64Util.java
+++ app/src/main/java/io/ns/sinduk/utils/Base64Util.java
@@ -2,15 +2,30 @@ package io.ns.sinduk.utils;
 
 import java.util.Base64;
 
+/**
+ * A utility to encode or decode given payload based on base64 scheme
+ */
 public class Base64Util {
 
     private static Base64.Encoder encoder = Base64.getEncoder();
     private static Base64.Decoder decoder = Base64.getDecoder();
 
+    /**
+     * Encode the given byte array
+     *
+     * @param payload The byte array to be encoded as string
+     * @return The base64 encoded string
+     */
     public static String encodeToString(byte[] payload) {
         return encoder.encodeToString(payload);
     }
 
+    /**
+     * Decode the base64 encoded string
+     *
+     * @param payload The string to be decoded
+     * @return The decoded byte array
+     */
     public static byte[] decodeToBytes(String payload) {
         return decoder.decode(payload);
     }
